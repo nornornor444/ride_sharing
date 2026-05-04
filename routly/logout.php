9e1e9e19bb57accf0330d6bb6b16d978d5c9e3c3
nornornor444 <arafikabbo@gmail.com>
2026-05-04 14:13:52 +0600

    Refactor logout.php for improved clarity

--- routly/logout.php
+++ routly/logout.php
@@ -1,14 +1,14 @@
 <?php
-// 1. Resume the current session
+
 session_start();
 
-// 2. Unset all of the session variables
+
 $_SESSION = array();
 
-// 3. Destroy the session entirely
+
 session_destroy();
 
-// 4. Redirect the user back to the landing page
+
 header("Location: index.html");
 exit();
 ?>
